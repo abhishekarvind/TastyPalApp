8c0d4cbd831613f7b3406787cb269ec911f292b0
abhishekarvind <abhishekarvind07@gmail.com>
2024-03-14 23:12:45 +0530

    minor logic correction done

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FirebaseAuth.instance.currentUser != null?const Mainhome():const GetStarted(),
+      home:FirebaseAuth.instance.currentUser != null?const Mainhome():const GetStarted(),
       theme: ThemeData(
         dialogBackgroundColor:  CustomColor.lightgreen(),
         scaffoldBackgroundColor: CustomColor.lightgreen(),
